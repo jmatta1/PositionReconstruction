38c80fafce0d7f659fdbb98a2579672fc57441fd
James Till Matta <jamesmatta@gmail.com>
2017-09-10 12:29:10 -0400

    Wrote Detector Position storage class
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Input/DetectorPositionData.cpp
+++ src/Input/DetectorPositionData.cpp
@@ -1,1 +1,69 @@
 #include"DetectorPositionData.h"
+
+// includes for C system headers
+// includes for C++ system headers
+#include<iostream>
+#include<iomanip>
+// includes from other libraries
+// includes from this code
+
+bool DetectorPositionData::validate()
+{
+    bool notEmpty = ((detectorNumber.size() > 0) &&
+                     (runNumber.size() > 0) &&
+                     (xPosition.size() > 0) &&
+                     (yPosition.size() > 0) &&
+                     (zPosition.size() > 0));
+    
+    int tempSize = detectorNumber.size();
+    bool equalSize = ((runNumber.size() == tempSize) &&
+                      (xPosition.size() == tempSize) &&
+                      (yPosition.size() == tempSize) &&
+                      (zPosition.size() == tempSize));
+
+    return (notEmpty && equalSize);
+}
+
+void DetectorPositionData::printValidation()
+{
+    bool notEmpty = ((detectorNumber.size() > 0) &&
+                     (runNumber.size() > 0) &&
+                     (xPosition.size() > 0) &&
+                     (yPosition.size() > 0) &&
+                     (zPosition.size() > 0));
+    
+    int tempSize = detectorNumber.size();
+    bool equalSize = ((runNumber.size() == tempSize) &&
+                      (xPosition.size() == tempSize) &&
+                      (yPosition.size() == tempSize) &&
+                      (zPosition.size() == tempSize));
+    if(notEmpty)
+    {
+        std::cout << "There must be at least one entry in each column" << std::endl;
+    }
+    
+    if(equalSize)
+    {
+        std::cout << "Each column must have the same number of entries" << std::endl;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const DetectorPositionData& dpd)
+{
+    os << "| Detector Number | Run Number | X Position | Y Position | Z Position |" << std::endl;
+    os << "|-----------------|------------|------------|------------|------------|" << std::endl;
+    
+    int size = dpd.detectorNumber.size();
+    
+    for(int i=0; i<size; ++i)
+    {
+        os <<  "| " << std::setw(15) << dpd.detectorNumber[i];
+        os << " | " << std::setw(10) << dpd.runNumber[i];
+        os << " | " << std::setw(10) << std::setprecision(1) << dpd.xPosition[i];
+        os << " | " << std::setw(10) << std::setprecision(1) << dpd.yPosition[i];
+        os << " | " << std::setw(10) << std::setprecision(1) << dpd.zPosition[i];
+        os << " |";
+    }
+    
+    return os << "|-----------------|------------|------------|------------|------------|";
+}
--- src/Input/DetectorPositionData.h
+++ src/Input/DetectorPositionData.h
@@ -1,5 +1,33 @@
 #ifndef POSITIONRECONSTRUCTION_SRC_INPUT_DETECTORPOSITIONDATA_H
 #define POSITIONRECONSTRUCTION_SRC_INPUT_DETECTORPOSITIONDATA_H
 
+// includes for C system headers
+// includes for C++ system headers
+#include<vector>
+// includes from other libraries
+// includes from this code
+
+class DetectorPositionData
+{
+public:
+    std::vector<int> detectorNumber;
+    std::vector<int> runNumber;
+    std::vector<float> xPosition;
+    std::vector<float> yPosition;
+    std::vector<float> zPosition;
+
+    void addDetectorNumber(int input){detectorNumber.push_back(input);}
+    void addRunNumber(int input){runNumber.push_back(input);}
+    void addXPosition(float input){xPosition.push_back(input);}
+    void addYPosition(float input){yPosition.push_back(input);}
+    void addZPosition(float input){zPosition.push_back(input);}
+
+    bool validate();
+    void printValidation();
+
+    friend std::ostream& operator<<(std::ostream& os, const DetectorPositionData& dpd);
+private:
+
+};
 
 #endif //POSITIONRECONSTRUCTION_SRC_INPUT_DETECTORPOSITIONDATA_H
