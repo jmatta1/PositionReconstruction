af9eeb4503ab3d066a4226b3ff0043e2df61c856
James Till Matta <jamesmatta@gmail.com>
2017-09-09 21:50:18 -0400

    Wrote the config data storage class
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Input/ConfigData.cpp
+++ src/Input/ConfigData.cpp
@@ -1,1 +1,48 @@
 #include"ConfigData.h"
+
+// includes for C system headers
+// includes for C++ system headers
+#include<iostream>
+// includes from other libraries
+// includes from this code
+
+void ConfigData::printValidationErrors()
+{
+    if(setDetSpecInputFile_)
+    {
+        std::cout << "The detector spectrum input file was not set" << std::endl;
+    }
+    if(setDetPosInputFile_)
+    {
+        std::cout << "The detector position input file was not set" << std::endl;
+    }
+    if(setPanelPosInputFile_)
+    {
+        std::cout << "The AD panel input file was not set" << std::endl;
+    }
+    if(setPanelSpecOutputFile_)
+    {
+        std::cout << "The panel spectrum output file was not set" << std::endl;
+    }
+    if(setSrcDataOutputFile_)
+    {
+        std::cout << "The source data output file was not set." << std::endl;
+    }
+    if(setSetNumCores_)
+    {
+        std::cout << "The number of cores was not set." << std::endl;
+    }
+}
+
+
+std::ostream& operator<<(std::ostream& os, const ConfigData& cd)
+{
+    os << "[Start Configuration Input]\n"
+       << "    Detector Spectrum Input File  = " << cd.detSpecInputFile    << "\n"
+       << "    Detector Position Input File  = " << cd.detPosInputFile     << "\n"
+       << "    AD Panel Input File           = " << cd.panelPosInputFile   << "\n"
+       << "    Panel Spectrum Output File    = " << cd.panelSpecOutputFile << "\n"
+       << "    Source Data Ouput File        = " << cd.srcDataOutputFile   << "\n"
+       << "    Number of Cores               = " << cd.numCores            << "\n";
+    return os << "[End Configuration Input]";
+}
--- src/Input/ConfigData.h
+++ src/Input/ConfigData.h
@@ -1,5 +1,43 @@
 #ifndef POSITIONRECONSTRUCTION_SRC_INPUT_CONFIGDATA_H
 #define POSITIONRECONSTRUCTION_SRC_INPUT_CONFIGDATA_H
 
+// includes for C system headers
+// includes for C++ system headers
+#include<string>
+// includes from other libraries
+// includes from this code
+
+
+
+class ConfigData
+{
+public:
+    std::string detSpecInputFile;
+    std::string detPosInputFile;
+    std::string panelPosInputFile;
+    std::string panelSpecOutputFile;
+    std::string srcDataOutputFile;
+    int numCores;
+
+    void setDetSpecInputFile(const std::string& input){detSpecInputFile = input; setDetSpecInputFile_ = true;}
+    void setDetPosInputFile(const std::string& input){detPosInputFile = input; setDetPosInputFile_ = true;}
+    void setPanelPosInputFile(const std::string& input){panelPosInputFile = input; setPanelPosInputFile_ = true;}
+    void setPanelSpecOutputFile(const std::string& input){panelSpecOutputFile = input; setPanelSpecOutputFile_ = true;}
+    void setSrcDataOutputFile(const std::string& input){srcDataOutputFile = input; setSrcDataOutputFile_ = true;}
+    void setSetNumCores(int input){numCores = input; setNumCores_ = true;}
+    
+    bool validate(){return (setDetSpecInputFile_ && setDetPosInputFile_ && setPanelPosInputFile_ &&
+                            setPanelSpecOutputFile_ && setSrcDataOutputFile_ && setSetNumCores_);}
+    void printValidationErrors();
+
+    friend std::ostream& operator<<(std::ostream& os, const ConfigData& cd);
+private:
+    bool setDetSpecInputFile_;
+    bool setDetPosInputFile_;
+    bool setPanelPosInputFile_;
+    bool setPanelSpecOutputFile_;
+    bool setSrcDataOutputFile_;
+    bool setSetNumCores_;
+};
 
 #endif //POSITIONRECONSTRUCTION_SRC_INPUT_CONFIGDATA_H
