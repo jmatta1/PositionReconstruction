69d07782bad3f84dc7256cacbe43d4e076549cfd
James Till Matta <jamesmatta@gmail.com>
2017-09-11 15:42:37 -0400

    Very basic position spectrum class
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Reconstruction/PositionSpectrum.h
+++ src/Reconstruction/PositionSpectrum.h
@@ -1,5 +1,21 @@
 #ifndef POSITIONRECONSTRUCTION_SRC_RECONSTRUCTION_POSITIONSPECTRUM_H
 #define POSITIONRECONSTRUCTION_SRC_RECONSTRUCTION_POSITIONSPECTRUM_H
 
+namespace Reconstruction
+{
+
+struct PositionSpectrum
+{
+public:
+    PositionSpectrum(int numPos, double en):positionCount(numPos), centerEnergy(en) {spectrum = new double[numPos];}
+    ~PositionSpectrum(){delete[] spectrum;}
+    
+
+    int positionCount;
+    double centerEnergy;
+    double* spectrum;
+};
+
+}
 
 #endif //POSITIONRECONSTRUCTION_SRC_RECONSTRUCTION_POSITIONSPECTRUM_H
