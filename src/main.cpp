7882bc46c3f29978ad2fad399125585dffc543c7
James Till Matta <jamesmatta@gmail.com>
2017-09-11 10:08:35 -0400

    Use written parser
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 
+#include"Input/Data/ConfigData.h"
+#include"Input/ParseFunctions.h"
+
+
 int main(int argc, char* argv[])
 {
     if( argc != 2 )
@@ -7,7 +11,12 @@ int main(int argc, char* argv[])
         std::cout << "Usage:\n\t" << argv[0] << " <primary configuration file>"<<std::endl;
         return 0;
     }
-    std::cout << "Reading primary configuration from: " << argv[1] << std::endl;
+    Input::Data::ConfigData config;
+    std::string configFileName(argv[1]);
+    if(!Input::parseValAndPrintConfigFile(&config, configFileName, std::cout))
+    {
+        std::cout << "Could not read input, exitting: " << std::endl;
+    }
     
     
     return 0;
