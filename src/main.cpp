5dd6b380fa0460726eca89b765c1bb7cc2d6cb7b
James Till Matta <jamesmatta@gmail.com>
2017-09-11 14:42:24 -0400

    Add reading of all input
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,57 @@
 #include<iostream>
 
 #include"Input/Data/ConfigData.h"
+#include"Input/Data/DetectorPositionData.h"
+#include"Input/Data/PanelPositionData.h"
 #include"Input/ParseFunctions.h"
 
 
 int main(int argc, char* argv[])
 {
+    //check for the appropriate number of command line arguments
     if( argc != 2 )
     {
         std::cout << "Usage:\n\t" << argv[0] << " <primary configuration file>"<<std::endl;
-        return 0;
+        return 1;
     }
+    std::cout<<"\n";
     Input::Data::ConfigData config;
     std::string configFileName(argv[1]);
+    //read and output the primary input file
     if(!Input::parseValAndPrintConfigFile(&config, configFileName, std::cout))
     {
-        std::cout << "Could not read input, exitting: " << std::endl;
+        std::cout << "Primary Configuration File Input Error, Quitting" << std::endl;
+        return 1;
+    }
+    else
+    {
+        std::cout << config << std::endl;
     }
     
+    std::cout<<"\n\n";
+    //read and output the detector position file
+    Input::Data::DetectorPositionData detPos;
+    if(!Input::parseValAndPrintDetectorPositionFile(&detPos, config.detPosInputFile, std::cout))
+    {
+        std::cout << "Detector Position File Input Error, Quitting" << std::endl;
+        return 1;
+    }
+    else
+    {
+        std::cout << detPos << std::endl;
+    }
+    std::cout<<"\n\n";
+    //read and output the panel position file
+    Input::Data::PanelPositionData panPos;
+    if(!Input::parseValAndPrintPanelPositionFile(&panPos, config.panelPosInputFile, std::cout))
+    {
+        std::cout << "Panel Position File Input Error, Quitting" << std::endl;
+        return 1;
+    }
+    else
+    {
+        std::cout << panPos << std::endl;
+    }
     
     return 0;
 }
