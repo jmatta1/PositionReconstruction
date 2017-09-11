5a946f9b84841ac972b274c855ad83bca7e535fb
James Till Matta <jamesmatta@gmail.com>
2017-09-11 14:41:57 -0400

    Added convenience functions for use of parsers
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Input/ParseFunctions.cpp
+++ src/Input/ParseFunctions.cpp
@@ -1,9 +1,15 @@
 #include"ParseFunctions.h"
-
+// includes for C system headers
+// includes for C++ system headers
 #include<fstream>
-
+// includes from other libraries
+// includes from this code
 #include"Data/ConfigData.h"
+#include"Data/DetectorPositionData.h"
+#include"Data/PanelPositionData.h"
 #include"Parsers/ConfigParser.h"
+#include"Parsers/DetectorPositionParser.h"
+#include"Parsers/PanelPositionParser.h"
 
 namespace Input
 {
@@ -38,13 +44,79 @@ bool parseValAndPrintConfigFile(Data::ConfigData* inParams,
     if(!inParams->validate())
     {
         os << "Could not validate configuration data" << std::endl;
-        inParams->printValidationErrors();
+        inParams->printValidationErrors(os);
+        return false;
+    }
+    return true;
+}
+
+bool parseDetectorPositionFile(Data::DetectorPositionData* inParams, const std::string& inputFileName)
+{
+    //make the parser, this line will make the compiler slow down a lot
+    Parsers::DetectorPositionParser<It> detectorPositionParser(inParams);
+    //set up the file
+    std::ifstream input(inputFileName);
+    //unset the skip whitespace
+    input.unsetf(std::ios::skipws);
+    //set up the iterators
+    It start(input), stop;
+    //parse the input
+    return boost::spirit::qi::parse(start, stop, detectorPositionParser);
+}
+
+bool parseValAndPrintDetectorPositionFile(Data::DetectorPositionData* inParams,
+                                          const std::string& inputFileName, std::ostream& os)
+{
+    os << "Reading Configuration Data from: " << inputFileName << std::endl;
+    bool parseSuccess = parseDetectorPositionFile(inParams, inputFileName);
+    if(!parseSuccess)
+    {
+        os<<"Could not parse "<<inputFileName<<std::endl;
         return false;
     }
     
-    os << "Input configuration is:"<<std::endl;
-    os << *inParams << std::endl;
+    if(!inParams->validate())
+    {
+        os << "Could not validate configuration data" << std::endl;
+        inParams->printValidationErrors(os);
+        return false;
+    }
     return true;
 }
+
+bool parsePanelPositionFile(Data::PanelPositionData* inParams, const std::string& inputFileName)
+{
+    //make the parser, this line will make the compiler slow down a lot
+    Parsers::PanelPositionParser<It> detectorPositionParser(inParams);
+    //set up the file
+    std::ifstream input(inputFileName);
+    //unset the skip whitespace
+    input.unsetf(std::ios::skipws);
+    //set up the iterators
+    It start(input), stop;
+    //parse the input
+    return boost::spirit::qi::parse(start, stop, detectorPositionParser);
+}
+
+bool parseValAndPrintPanelPositionFile(Data::PanelPositionData* inParams,
+                                       const std::string& inputFileName, std::ostream& os)
+{
+    os << "Reading Configuration Data from: " << inputFileName << std::endl;
+    bool parseSuccess = parsePanelPositionFile(inParams, inputFileName);
+    if(!parseSuccess)
+    {
+        os<<"Could not parse "<<inputFileName<<std::endl;
+        return false;
+    }
+    
+    if(!inParams->validate())
+    {
+        os << "Could not validate configuration data" << std::endl;
+        inParams->printValidationErrors(os);
+        return false;
+    }
+    return true;
+}
+
 
 }
--- src/Input/ParseFunctions.h
+++ src/Input/ParseFunctions.h
@@ -1,8 +1,13 @@
 #ifndef POSITIONRECONSTRUCTION_SRC_INPUT_PARSEFUNCTIONS_H
 #define POSITIONRECONSTRUCTION_SRC_INPUT_PARSEFUNCTIONS_H
 
+
+// includes for C system headers
+// includes for C++ system headers
 #include<string>
 #include<ostream>
+// includes from other libraries
+// includes from this code
 
 namespace Input
 {
@@ -10,12 +15,24 @@ namespace Input
 namespace Data
 {
 class ConfigData;
+class DetectorPositionData;
+class PanelPositionData;
 }
 
 bool parseConfigFile(Data::ConfigData* inParams, const std::string& inputFileName);
 
 bool parseValAndPrintConfigFile(Data::ConfigData* inParams,
                                 const std::string& inputFileName, std::ostream& os);
+
+bool parseDetectorPositionFile(Data::DetectorPositionData* inParams, const std::string& inputFileName);
+
+bool parseValAndPrintDetectorPositionFile(Data::DetectorPositionData* inParams,
+                                          const std::string& inputFileName, std::ostream& os);
+
+bool parsePanelPositionFile(Data::PanelPositionData* inParams, const std::string& inputFileName);
+
+bool parseValAndPrintPanelPositionFile(Data::PanelPositionData* inParams,
+                                       const std::string& inputFileName, std::ostream& os);
 
 }
 #endif //POSITIONRECONSTRUCTION_SRC_INPUT_PARSEFUNCTIONS_H
