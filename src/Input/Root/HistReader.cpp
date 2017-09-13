b52cb0032e073a6b4eed903316a9e7d4d5c89da9
James Till Matta <jamesmatta@gmail.com>
2017-09-13 10:08:56 -0400

    First pass at building the histogram reader
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- src/Input/Root/HistReader.cpp
+++ src/Input/Root/HistReader.cpp
@@ -1,1 +1,60 @@
 #include"HistReader.h"
+// includes for C system headers
+// includes for C++ system headers
+#include<sstream>
+// includes from other libraries
+#include<TFile.h>
+#include<TH1.h>
+// includes from this code
+#include"Input/Data/DetectorPositionData.h"
+#include"Reconstruction/DetectorSet.h"
+
+namespace Input
+{
+
+Reconstruction::DetectorSet* HistReader::readPositionSpectra()
+{
+    TFile* inFile = new TFile(fileName.c_str());
+    
+    //first read the first histogram just to get the number of energy bins so
+    //that we can construct the detector set
+    buildSpecName(detPosData->detectorNumber[0], detPosData->runNumber[0]);
+    TH1D* hist = (TH1D*)inFile->Get(specName.c_str());
+    int loBin = hist->FindBin(minEn);
+    int hiBin = hist->FindBin(maxEn);
+    int numBins = (hiBin-loBin+1);
+    //now construct the detector set
+    int numRuns = detPosData->runNumber.size();
+    Reconstruction::DetectorSet* output = new Reconstruction::DetectorSet(numBins, numRuns);
+    //load the energy centers
+    for(int j=loBin; j<=hiBin; ++j)
+    {
+        output->setEnergy(j-loBin, hist->GetBinCenter(j));
+    }
+    delete hist;
+    //now load each histogram, set the position, and set the position spectra counts
+    for(int i=0; i<numRuns; ++i)
+    {
+        buildSpecName(detPosData->detectorNumber[i], detPosData->runNumber[i]);
+        hist = (TH1D*)inFile->Get(specName.c_str());
+        //set the position for this particular position bin
+        output->setPosition(i, detPosData->xPosition[i],
+                            detPosData->yPosition[i], detPosData->zPosition[i]);
+        for(int j=0; j < numBins; ++j)
+        {
+            output->setPosBin(i, j, hist->GetBinContent(j+loBin));
+        }
+        delete hist;
+    }
+    delete inFile;
+    return output;
+}
+
+void HistReader::buildSpecName(int detNum, int runNum)
+{
+    std::ostringstream namer;
+    namer << "Decomp_" << detNum << "_" << runNum;
+    specName = namer.str();
+}
+
+}
--- src/Input/Root/HistReader.h
+++ src/Input/Root/HistReader.h
@@ -1,5 +1,42 @@
 #ifndef POSITIONRECONSTRUCTION_SRC_INPUT_ROOT_HISTREADER_H
 #define POSITIONRECONSTRUCTION_SRC_INPUT_ROOT_HISTREADER_H
+// includes for C system headers
+// includes for C++ system headers
+#include<string>
+// includes from other libraries
+// includes from this code
 
+namespace Reconstruction
+{
+class DetectorSet;
+}
 
+namespace Input
+{
+
+namespace Data
+{
+class DetectorPositionData;
+}
+
+class HistReader
+{
+public:
+    HistReader(const std::string& fName, Data::DetectorPositionData* detPosDat, float minEnergy, float maxEnergy):
+        fileName(fName), detPosData(detPosDat), minEn(minEnergy), maxEn(maxEnergy){}
+    
+    Reconstruction::DetectorSet* readPositionSpectra();
+    
+private:
+    
+    void buildSpecName(int detNum, int runNum);
+    std::string specName;
+    
+    std::string fileName;
+    Data::DetectorPositionData* detPosData;
+    float minEn;
+    float maxEn;
+};
+
+}
 #endif //POSITIONRECONSTRUCTION_SRC_INPUT_ROOT_HISTREADER_H
